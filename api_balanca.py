15b6127085d73802a8648397ab272b4744a15d67
heliohenriquehfilho@gmail.com <heliohenriquehfilho@gmail.com>
2024-11-27 10:19:42 -0300

    api_Balanca update com3

--- api_balanca.py
+++ api_balanca.py
@@ -5,9 +5,19 @@ import time
 app = Flask(__name__)
 
 # Configuração da porta serial (ajuste a COM do seu Arduino, por exemplo, 'COM3' ou '/dev/ttyUSB0')
-SERIAL_PORT = "COM3"  # Substitua com a porta do seu Arduino
 BAUD_RATE = 57600
 SERIAL_TIMEOUT = 1  # Timeout para a leitura da porta serial
+
+import os
+
+SERIAL_PORT = os.getenv('SERIAL_PORT', 'COM3')  # Usa COM3 por padrão, mas pode ser sobreposto via variável de ambiente
+
+# Verifica se a porta serial está configurada para algo válido
+if SERIAL_PORT and SERIAL_PORT != 'COM3':
+    ser = serial.Serial(SERIAL_PORT, 57600, timeout=1)
+else:
+    # Lógica para rodar sem a porta serial
+    print("A porta serial não está disponível neste ambiente.")
 
 # Abrir a porta serial
 ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=SERIAL_TIMEOUT)
